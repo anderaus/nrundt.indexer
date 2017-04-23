c4bc10b267caaff05f3a1e70a0cb4868d532a086
Anders Austad <anderaus@outlook.com>
2017-04-23 18:07:57 +0200

    Add project files.

---
+++ nrundt.indexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nrundt.indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
