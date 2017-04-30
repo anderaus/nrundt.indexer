68ee8c3ace97d1fb4af0ae49585c7328a87f74e0
Anders Austad <anderaus@gmail.com>
2017-04-30 12:43:40 +0200

    Make project mac- and vscode-friendly

--- nrundt.indexer/Program.cs
+++ nrundt.indexer/Program.cs
@@ -10,11 +10,14 @@ namespace nrundt.indexer
             Log("It's on!");
 
             // Parse CSV file
-            var parser = new Parser(@"..\resources\norge_rundt_statistikkmoro_2016_subset.csv");
+            var parser = new Parser(@"../resources/norge_rundt_statistikkmoro_2016_subset.csv");
             var result = parser.GetResult<Sequence, SequenceMap>().ToList();
             Log($"Got {result.Count} sequences");
 
             // TODO: Index each episode segment
+            foreach (var seq in result) {
+                Log(seq.Title);
+            }
 
             Log("...and we're done");
         }
